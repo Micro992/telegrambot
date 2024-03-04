b9f9fb8f0f273366f86e14c0d977bf0ce6bc2f22
micro <jokerlraq777@gmail.com>
2024-03-04 17:36:04 +0300

    editbug

--- telegtambot.py
+++ telegtambot.py
@@ -4,12 +4,7 @@ from PIL import Image
 from telegram import Bot, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
-<<<<<<< HEAD
 TOKEN = "YOUR_TOKEN"
-=======
-TOKEN = "Your_TOKEN"
->>>>>>> 205c0cad1cb4ebf53224d858c6d9769763e433de
-
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id = update.effective_chat.id, text = 'hello please click here /start')
